5140df958a53aa2dc80d4ef125c3359600b31dc7
David Hamilton <daloach20@gmail.com>
2020-04-28 07:17:49 -0500

    merged changes together into 1 branch (difficult decisions were made :( )

--- mobile_wallet_app/lib/screens/splash_screen.dart
+++ mobile_wallet_app/lib/screens/splash_screen.dart
@@ -2,63 +2,99 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 // import 'package:mobile_wallet_app/main.dart';
 import 'package:mobile_wallet_app/screens/home_screen.dart';
 import 'package:mobile_wallet_app/screens/signin_screen.dart';
+
+
+//import 'package:mobile_wallet_app/screens/signin_screen.dart';
 // import 'package:mobile_wallet_app/data/current_session.dart';
-
-class SplashScreen extends StatelessWidget {
-
+class SplashScreen extends StatefulWidget {
   static const String id = "LOADING";
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
 
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5),
+        () => Navigator.of(context).pushNamed(SignInScreen.id));
+  }
+
+  // Navigator.of(context).pushNamed(SignInScreen.id);
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.of(context).pushNamed(SignInScreen.id);
-    });
 
-      return Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Center(
-          child: Container(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(36.0),
-              child: SingleChildScrollView(
-                child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    height: 155.0,
-                    child: Image.asset(
-                      "lib/assets/dw.png",
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                  SizedBox(height: 45.0,
-                    child: Text('Mobile Wallet', style: TextStyle(
-                        fontFamily: 'Monsterrat', fontSize: 40.0, color: Colors.deepPurple, fontWeight: FontWeight.bold)
-                      ),
-                    ),
-                  SizedBox(height: 25.0,
-                    child: Text('Loading...', style: TextStyle(
-                      fontFamily: 'Monsterrat', fontSize: 20.0, color: Colors.deepPurpleAccent, fontWeight: FontWeight.bold)
-                      ),
-                   ),
-                  SizedBox(height: 140.0,),
-                  SizedBox(height: 30.0,
-                      child: Text('Gathering Resources...', style: TextStyle(
-                        fontFamily: 'Monsterrat', fontSize: 20.0, color: Colors.deepPurpleAccent, fontWeight: FontWeight.bold)
-                      ),
-                    ),
-                  SizedBox(height: 5.0),
-                ],
-              )
+
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.deepPurpleAccent),
           ),
-        ),
-      ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundColor: Colors.black,
+                          radius: 50.0,
+                          child: Icon(
+                            Icons.phone_android,
+                            color: Colors.white,
+                            size: 50.0,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10.0),
+                        ),
+                        Text(
+                          "DigiWallet",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24.0),
+                        ),
+                      ]),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(
+                      backgroundColor: Colors.black,
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Text(
+                      "For all your money storing needs!",
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
 
